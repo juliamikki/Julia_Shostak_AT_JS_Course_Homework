d387e12fb1b89ac5a457695b27761b3ef01060dc
Julia Shostak <jshostak@ibagroup.eu>
2026-02-10 11:14:27 +0300

    adding the recursive functions to the fibonacci assignment

--- hw6/fibonacci.js
+++ hw6/fibonacci.js
@@ -41,3 +41,35 @@ function createFibonacciFrom(startIndex, arrayLength) {
 console.log(createFibonacciWithForLoop(10));
 console.log(createFibonacciWithReduce(10));
 console.log(createFibonacciFrom(7, 15));
+
+
+//new functions from the classwork insights:
+function createFibonacciElementWithRecursive(x) {
+  if (x <= 1) {
+    return x;
+  } else
+    return (createFibonacciElementWithRecursive(x - 2) + createFibonacciElementWithRecursive(x - 1));
+}
+
+function createFibonacciElementWithRecursiveAndMemo(x, memorized = {}) {
+  if (memorized[x] !== undefined) {
+    return memorized[x];
+  }
+  if (x <= 1) {
+    return x;
+  }
+  memorized[x] = createFibonacciElementWithRecursiveAndMemo(x - 2) + createFibonacciElementWithRecursiveAndMemo(x - 1);
+  return memorized[x];
+}
+
+function createFibonacciSequenceWithRecursiveAndMemo(startIndex, arrayLength) {
+  const array = [];
+  for (let i = 0; i <= arrayLength; i++) {
+    array.push(createFibonacciElementWithRecursiveAndMemo(i));
+  }
+  return array.slice(startIndex);
+}
+
+console.log(createFibonacciElementWithRecursive(13))
+console.log(createFibonacciElementWithRecursiveAndMemo(13));
+console.log(createFibonacciSequenceWithRecursiveAndMemo(4, 13));
