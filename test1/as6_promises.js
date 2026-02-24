7700ada578b86e25ce821d4b6cc3924f7892dcf5
Julia Shostak <jshostak@ibagroup.eu>
2026-02-24 11:22:57 +0300

    promises fixes

--- test1/as6_promises.js
+++ test1/as6_promises.js
@@ -2,57 +2,27 @@ function getRandom(min = 1, max = 10) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//solution #1:
-await new Promise((resolve) => {
-  console.log("/========= Solution 1: =========/");
-  setTimeout(() => {
-    const number = getRandom();
-    console.log("First number:", number);
-    resolve(number);
-  }, 3000);
-}).then((result) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const squared = result * result;
-      console.log("Squared number:", squared);
-      resolve(squared);
-    }, 3000);
-  }).then((result) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const fourthPower = result * result;
-        console.log("Fouth power number:", fourthPower);
-        resolve(fourthPower);
-      }, 3000);
-    });
-  });
-});
-
-//solution #2 (scquareNumber func incapsulated):
 function squareNumber(number) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const squared = number * number;
-      resolve(squared);
+      resolve(number * number);
     }, 3000);
   });
 }
 
 new Promise((resolve) => {
-  console.log("/========= Solution 2: =========/");
   setTimeout(() => {
     const number = getRandom();
     console.log("First number:", number);
     resolve(number);
   }, 3000);
 })
-  .then((result) => {
-    return squareNumber(result);
-  })
+  .then(squareNumber)
   .then((result) => {
     console.log("Squared number:", result);
     return squareNumber(result);
   })
   .then((result) => {
     console.log("Fouth power number:", result);
-  });
+  })
+  .catch((err) => console.error("Something went wrong:", err.message));
