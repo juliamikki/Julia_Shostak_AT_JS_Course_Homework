2d892d96928d129c525eafa35876861bb0a1dc32
Julia Shostak <jshostak@ibagroup.eu>
2026-02-24 15:27:14 +0300

    assignment 4: promises inside async function

---
+++ test1/as4_asyncFunc.js
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+
+function createPromise(id) {
+  return new Promise((resolve, reject) => {
+    if (Math.random() < 0.5) {
+      resolve(`Here is promise #${id}!`);
+    } else reject(`Here is promise #${id}!`);
+  });
+}
+
+async function run(funcArray) {
+  const results = await Promise.allSettled(funcArray);
+  results.forEach((result, index) => {
+    const _ =
+      result.status === "fulfilled"
+        ? console.log(`Promise ${index + 1} is ${result.status} : ${chalk.green(result.value)}`)
+        : console.log(`Promise ${index + 1} is ${result.status} : ${chalk.red(result.reason)}`);
+  });
+}
+
+run([createPromise(2), createPromise(5), createPromise(3), createPromise(4), createPromise(1)]);
